c2b3d6b91c42f0c8ef2cc19c821d3f3aedbcc11d
melina <melinamora42@gmail.com>
2019-09-05 22:52:03 +0200

    Update in json and some tests in jobsite request Jobsite request class

--- tests/tools/json_tools.py
+++ tests/tools/json_tools.py
@@ -1,24 +1,35 @@
-from jsonpath_ng import parse
+import jsonpath_ng
 import json
 
 
-class Parser:
-
-    @staticmethod
-    def string_to_json(source):
-        try:
+def string_to_json(source):
+    try:
+        if source is type(str):
             load_input_json = json.loads(source)
-        except ValueError as e:
-            raise Exception("Could not parse '%s' as JSON: %s"%(source, e))
+            return load_input_json
+        elif source is type(dict):
+            load_input_json = json.dumps(source)
+            return load_input_json
+        else:
+            return source
+    except ValueError as e:
+        raise Exception("Could not parse '%s' as JSON: %s"%(source, e))
 
-        return load_input_json
 
-    @staticmethod
-    def update_json(body, path, new_value, index=0):
-        jsonpath_expr = parse(path)
-        result = jsonpath_expr.update(body, new_value)
+def update_json(body, path, new_value):
+    body = string_to_json(body)
+    jsonpath_expr = jsonpath_ng.parse(path)
+    result = jsonpath_expr.update(body, new_value)
+    return result
+
+
+def fetch_value_from_json(body, path, multiple=False):
+    jsonpath_expr = jsonpath_ng.parse(path)
+    result = [match.value for match in jsonpath_expr.find(body)]
+    if multiple:
         return result
-
+    else:
+        return result[0]
 
 
 
